0aa86fb6c171f8ad2aa03cccddb5d9cb1b13914f
Arun Gokul G R <arun-9689@arun-9689.csez.zohocorpin.com>
2020-07-23 19:11:56 +0530

    Added my project

---
+++ functions/basicio/BasicIO.java
@@ -0,0 +1,30 @@
+import com.catalyst.Context;
+import com.catalyst.basic.BasicIO;
+import com.catalyst.basic.ZCFunction;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import com.zc.common.ZCProject;
+import com.zc.component.cache.ZCCache;
+
+public class BasicIO implements ZCFunction {
+	private static final Logger LOGGER = Logger.getLogger(BasicIO.class.getName());
+	
+	@Override
+    public void runner(Context context, BasicIO basicIO) throws Exception {
+		try {
+			ZCProject.initProject();
+			String name = (String) basicIO.getParameter("name");
+			LOGGER.log(Level.INFO, "Hello "+name);
+			ZCCache.getInstance().putCacheValue("BasicIO", "Working", 1l);
+			basicIO.setStatus(200);
+		}
+		catch(Exception e) {
+			LOGGER.log(Level.SEVERE,"Exception in BasicIO",e);
+			basicIO.setStatus(500);
+		}
+        basicIO.write("Hello From BasicIO.java");
+	}
+	
+}
